c7e425c24d878241ab1d9e0262725a1c26751a71
chaseknis <chase6th@gmail.com>
2023-04-06 15:47:00 +0200

    Create a class MyList implement a method #each that yields successive members of @list and uses the MyEnumerable module Create a module MyEnumerable

--- index.rb
+++ index.rb
@@ -1,8 +1,22 @@
 module MyEnumerable
-  def my_each
-    for item in self
-      yield(item)
-    end
+  def my_all?
+    my_each { |item| return false unless yield(item) }
+    true
+  end
+
+  def my_any?
+    my_each { |item| return true if yield(item) }
+    false
+  end
+
+  def my_filter
+    result = []
+    my_each { |item| result << item if yield(item) }
+    result
+  end
+
+  def my_each(&block)
+    raise NotImplementedError, "You must implement the 'my_each' method"
   end
 end
 
@@ -13,7 +27,27 @@ class MyList
     @list = []
   end
 
-  def each
-    @list.my_each { |item| yield(item) }
+  def each(&block)
+    @list.each(&block)
+  end
+
+  def <<(item)
+    @list << item
+  end
+
+  def my_each(&block)
+    @list.each(&block)
   end
 end
+
+list = MyList.new
+list << 1
+list << 2
+list << 3
+list << 4
+
+puts(list.my_all? { |e| e < 5 })
+puts(list.my_all? { |e| e > 5 })
+puts(list.my_any? { |e| e == 2 })
+puts(list.my_any? { |e| e == 5 })
+puts(list.my_filter(&:even?))
