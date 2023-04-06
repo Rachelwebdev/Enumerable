b3601c530fa57a122eaa6894778fc930fb58ed05
chaseknis <chase6th@gmail.com>
2023-04-06 16:55:32 +0200

    Update index.rb ruby index.rb ruby index.rb

--- index.rb
+++ index.rb
@@ -1,6 +1,11 @@
 require_relative 'my_list'
 
-list = MyList.new(1, 2, 3, 4)
+list = MyList.new
+
+list << 1
+list << 2
+list << 3
+list << 4
 
 # Test #all?
 puts(list.my_all? { |e| e < 5 })
@@ -13,12 +18,11 @@ puts(list.my_any? { |e| e == 5 })
 # Test #filter
 puts(list.my_filter(&:even?))
 
-
 require_relative 'my_array'
 
 array = MyArray.new([1, 2, 3])
 
-puts array.my_all? { |item| item > 0 } 
-puts array.my_any? { |item| item == 4 } 
-puts array.my_filter { |item| item > 1 } 
-puts(list.my_filter(&:even?))
+puts array.my_all?(&:positive?)
+puts(array.my_any? { |item| item == 4 })
+puts(array.my_filter { |item| item > 1 })
+puts(array.my_filter(&:even?))
